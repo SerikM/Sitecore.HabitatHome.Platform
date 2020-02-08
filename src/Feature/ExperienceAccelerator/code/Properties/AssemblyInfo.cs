0dec44759ae1674a711def93a3e2386d4914bd20
SerikM <tyrkamotchka@mail.ru>
2020-02-08 22:44:03 +1100

    serialization fixed

--- src/Feature/ExperienceAccelerator/code/Properties/AssemblyInfo.cs
+++ src/Feature/ExperienceAccelerator/code/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.HabitatHome.Feature.XA.Extensions")]
+[assembly: AssemblyTitle("SxaBysoft.Feature.Hero")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sitecore.HabitatHome.Feature.XA.Extensions")]
+[assembly: AssemblyProduct("SxaBysoft.Feature.Hero")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
